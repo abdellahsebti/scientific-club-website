95103595f5e6b3415b7d6a21558ddfde98ef5528
abdellahsebti <abdellahsebti001@gmail.com>
2025-03-05 18:02:00 +0100

    added

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"J8w7za0MbCRLDfLpaHJGrkT/Jv78O8FsIrIXsirsz9M=\"\n}"
---
+++ .next/static/chunks/app_News_page_tsx_39ae1e._.js
@@ -0,0 +1,12 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/app_News_page_tsx_39ae1e._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/app_News_page_tsx_39ae1e._.js",
+  "chunks": [
+    "static/chunks/node_modules_next_dist_91fa56._.js",
+    "static/chunks/app_News_page_tsx_2b77ee._.js"
+  ],
+  "source": "dynamic"
+});
